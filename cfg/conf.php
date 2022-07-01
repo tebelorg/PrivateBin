4426d88a96c1719d870791dd12b22c8860232a5f
Ken Soh <kensoh@gmail.com>
2022-07-01 10:36:55 +0100

    Update conf.php

--- cfg/conf.php
+++ cfg/conf.php
@@ -12,7 +12,7 @@ name = "PrivateBin"
 ; basepath = ""
 
 ; enable or disable the discussion feature, defaults to true
-discussion = true
+discussion = false
 
 ; preselect the discussion feature, defaults to false
 opendiscussion = false
